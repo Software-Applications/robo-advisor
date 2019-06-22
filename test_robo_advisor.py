b208195d497d836244bbe9202221ce206e83ff2f
DheerajRekula <47189595+DheerajRekula@users.noreply.github.com>
2019-06-22 15:10:34 -0400

    Updated test_robo_advisor.py
    added test_get_response fucntion

--- test_robo_advisor.py
+++ test_robo_advisor.py
@@ -2,7 +2,7 @@
 
 import pytest
 import os
-from app.robo_advisor import to_usd, to_fnum
+from app.robo_advisor import to_usd, to_fnum, get_response
 
 
 
@@ -15,6 +15,16 @@ def test_to_usd():
 def test_fnum():
     assert to_fnum(1000) == "1,000"
     assert to_fnum(1000000) == "1,000,000"
+
+def test_get_response():
+    symbol = "NFLX"
+
+    parsed_response = get_response(symbol)
+
+    assert isinstance(parsed_response, dict)
+    assert "Meta Data" in parsed_response.keys()
+    assert "Weekly Time Series" in parsed_response.keys()
+    assert parsed_response["Meta Data"]["2. Symbol"] == symbol
 
 
 
